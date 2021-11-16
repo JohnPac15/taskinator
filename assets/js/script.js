d704065248833346945ec70eb81e04ebf836c8af
JohnPac <johnpac15@yahoo.com>
2021-11-16 00:24:38 -0600

    added eventlisteners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,31 @@
-var buttonEl = document.querySelector("#save-task");
-var taskToDoEl = document.querySelector("#tasks-to-do");
+// var formEl = document.querySelector("#save-task");
+// var taskToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandker = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "this is a new task. ";
-    taskToDoEl.appendChild(listItemEl);
-};
-buttonEl.addEventListener("click", createTaskHandker);
+// var createTaskHandker = function() {
+
+//     event.preventDefault();
 
 
-// var buttonEl = document.querySelector("#save-task"); 
-// var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-// var createTaskHandler = function() { 
-//   var listItemEl = document.createElement("li"); 
-//   listItemEl.className = "task-item"; 
-//   listItemEl.textContent = "This is a new task."; 
-//   tasksToDoEl.appendChild(listItemEl); 
-//   }; 
+//     var listItemEl = document.createElement("li");
+//     listItemEl.className = "task-item";
+//     listItemEl.textContent = "this is a new task. ";
+//     taskToDoEl.appendChild(listItemEl);
+    
+// };
+// formEl.addEventListener("submit", createTaskHandker);
 
-// buttonEl.addEventListener("click", createTaskHandler);
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
